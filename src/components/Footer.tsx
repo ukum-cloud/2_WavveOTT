b70e8e1feee8825862b38b3431c3902eb546e0d4
Oh Yeongseo <wnrdmadmlane@naver.com>
2025-12-10 18:40:15 +0900

    오영서 Footer tsx, scss 작업

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
         <div className="footer-item">
           <div className="footer-left">
             <div className="left-top">
-              <p>이용약관</p>
-              <p>개인정보처리방침</p>
-              <p>서비스 소개</p>
-              <p>이벤트</p>
+              <p className="font-russo">이용약관</p>
+              <p className="font-russo">개인정보처리방침</p>
+              <p className="font-russo">서비스 소개</p>
+              <p className="font-russo">이벤트</p>
             </div>
             <div className="left-bottom">
               <p>콘텐츠웨이브 주식회사 | 대표이사 서장호</p>
@@ -20,14 +20,12 @@ const Footer = () => {
           </div>
           <div className="footer-right">
             <div className="right-item">
-              <p className="item-title">고객센터</p>
+              <p className="item-title font-wave">고객센터</p>
               <p className="item-phone">1599-3709</p>
-              <div className="item-text">
-                <p className="work-hour">
-                  평일 09:00~17:00 / 점심시간 12:00~13:00 <br />
-                  주말 및 공휴일 휴무
-                </p>
-              </div>
+              <p className="work-hour">
+                평일 09:00~17:00 / 점심시간 12:00~13:00 <br />
+                주말 및 공휴일 휴무
+              </p>
             </div>
           </div>
         </div>
