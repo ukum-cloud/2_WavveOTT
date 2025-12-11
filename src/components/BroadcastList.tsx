dd691242c12674cc9da95d6fe99fc13c375aa372
Oh Yeongseo <wnrdmadmlane@naver.com>
2025-12-11 13:26:26 +0900

    오영서 작업

--- src/components/BroadcastList.tsx
+++ src/components/BroadcastList.tsx
@@ -4,15 +4,16 @@ interface LogoBtn{
   id: number;
   name: string;
   image: string;
+  src: string;
 }
 
 const broadcastBtns: LogoBtn[] = [
-  {id: 1, name: "mbc", image: "/images/badge/badge-mbc-logo.svg"},
-  {id: 2, name: "kbs", image: "/images/badge/badge-kbs-logo.svg"},
-  {id: 3, name: "jtbc", image: "/images/badge/badge-jtbc-logo.svg"},
-  {id: 4, name: "웹예능", image: "/images/badge/badge-웹예능-logo.svg"},
-  {id: 5, name: "cjenm", image: "/images/badge/badge-cjenm-logo.svg"},
-  {id: 6, name: "키즈", image: "/images/badge/badge-키즈-logo.svg"}
+  {id: 1, name: "mbc", image: "/images/badge/badge-mbc-logo.svg", src: ""},
+  {id: 2, name: "kbs", image: "/images/badge/badge-kbs-logo.svg", src: ""},
+  {id: 3, name: "jtbc", image: "/images/badge/badge-jtbc-logo.svg", src: ""},
+  {id: 4, name: "웹예능", image: "/images/badge/badge-웹예능-logo.svg", src: ""},
+  {id: 5, name: "cjenm", image: "/images/badge/badge-cjenm-logo.svg", src: ""},
+  {id: 6, name: "키즈", image: "/images/badge/badge-키즈-logo.svg", src: ""}
 ]
 
 const BroadcastList = () => {
