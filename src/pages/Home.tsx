a460be6a05756006d751d2dfc8126b216659c181
은희 <withjesus211@gmail.com>
2025-12-12 10:55:10 +0900

    KIM:홈정리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,25 +25,58 @@ const Home = () => {
   }, [onFetchWavve]);
 
   return (
-    <main className="main">
-      <div className="">100% 다 쓰는 경우</div>
-      <MainNomination />
-
-      <EditorRecommendCardList list={popularMovies} />
+    <main className="main-home">
+      <div className="" style={{ minHeight: "200px" }}>
+        메인 탑 비주얼 공간
+      </div>
       <div className="inner">
         <BroadcastList />
-        <section>1</section>
+        <section className="card-list">
+          <h2>지금 시청중인 컨텐츠</h2>
+          <div className="">내용, 슬라이더, 등등</div>
+        </section>
+      </div>
+
+      <MainNomination />
+
+      <div className="inner">
         <RankingCardList RankingData={popularMovies} />
         <WavveList title="오직 웨이브에서만" wavves={wavves} />
+        <div className="banner-event">
+          <Link to={"/"}>
+            <img
+              src="/images/banner/banner-main-event.png"
+              alt="banner event"
+            />
+          </Link>
+        </div>
         <section className="card-list">
           <h2>지금 주목받는 스타들</h2>
           <div className="">내용, 슬라이더, 등등</div>
         </section>
-        <div className="banner-event">
-          <Link to={"/"}>
-            <img src="/images/banner/banner-main-event.png" alt="banner event" />
-          </Link>
-        </div>
+        <section className="card-list">
+          <h2>이건 꼭 봐야해!</h2>
+          <div className="">내용, 슬라이더, 등등</div>
+        </section>
+        <section className="card-list">
+          <h2>NEW! 새로 올라온 영화</h2>
+          <div className="">내용, 슬라이더, 등등</div>
+        </section>
+      </div>
+      <EditorRecommendCardList list={popularMovies} />
+      <div className="inner">
+        <section className="card-list">
+          <h2>NEW! 새로 올라온 시리즈</h2>
+          <div className="">내용, 슬라이더, 등등</div>
+        </section>
+        <section className="card-list">
+          <h2>지금 방영중인 예능</h2>
+          <div className="">내용, 슬라이더, 등등</div>
+        </section>
+        <section className="card-list">
+          <h2>꼭 봐야하는 이슈</h2>
+          <div className="">내용, 슬라이더, 등등</div>
+        </section>
       </div>
     </main>
   );
