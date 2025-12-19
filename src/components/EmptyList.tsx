1e4b1e2fb9f29fc19cb08afe41ad5fcc9929c187
ukum <97ukum@gmail.com>
2025-12-19 11:57:53 +0900

    유겸 : EmptyList/ 찜기능 수정중

---
+++ src/components/EmptyList.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./scss/EmptyList.module.scss";
+
+const EmptyList = ({ title }: { title: string }) => {
+  return (
+    <div className={style.emptyWrap}>
+      <div className={style.emptyIcon}>
+        <img src="/images/icons/icon_empty.svg" alt="empty아이콘" />
+      </div>
+      <div className={style.title}>{title}</div>
+    </div>
+  );
+};
+
+export default EmptyList;
