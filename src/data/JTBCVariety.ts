42ea294175c42a15be53aa3d06f1fcc1249d6c91
jiin <amesystand@gmail.com>
2025-12-21 21:23:48 +0900

    Merge pull request #178 from ukum-cloud/JJI
    JJI: JTBC 페이지 추가

---
+++ src/data/JTBCVariety.ts
@@ -0,0 +1,554 @@
+import type { Wavves } from "./wavves";
+
+export const JTBCVariety: Wavves[] = [
+  {
+    index: 1,
+    series_title: "엄마, 단둘이 여행 갈래?",
+    series_refer_id: "J3801_J38000000056",
+    content_refer_id: "J3801_J38000000056_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "엄마, 단둘이 여행 갈래?",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "엄마, 단둘이 여행 갈래?",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202510/1760339767116610045.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202510/1760339774792817370.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202510/1760339784777442846.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202510/1760339784777442846.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202510/1760339821288138488.png",
+    targetage: "15",
+    actors: ["이효리"],
+    directors: ["마건영"],
+    tags: ["여행", "가족"],
+    season_writers: [],
+    tmdb_id: 255432,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 1st (series_title) (Used title: 엄마, 단둘이 여행 갈래?)",
+  },
+  {
+    index: 2,
+    series_title: "최강야구",
+    series_refer_id: "J3801_J38000000014",
+    content_refer_id: "J3801_J38000000016_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "최강야구",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "최강야구",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202508/1754543773813895834.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202508/1754539914514079095.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202508/1754543796312990885.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202508/1754543796312990885.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202508/1754543892820536288.png",
+    targetage: "15",
+    actors: [
+      "김성근",
+      "박용택",
+      "송승준",
+      "이택근",
+      "정성훈",
+      "심수창",
+      "이대호",
+      "정근우",
+      "장원삼",
+      "지석훈",
+      "서동욱",
+      "오주원",
+      "정의윤",
+      "유희관",
+      "김문호",
+      "이대은",
+      "이홍구",
+      "최수현",
+      "박찬희",
+      "김선우",
+      "정용검",
+    ],
+    directors: ["장시원"],
+    tags: ["스포츠"],
+    season_writers: [],
+    tmdb_id: 203552,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 1st (series_title) (Used title: 최강야구)",
+  },
+  {
+    index: 3,
+    series_title: "대놓고 두집살림",
+    series_refer_id: "J3801_J38000000075",
+    content_refer_id: "J3801_J38000000075_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "대놓고 두집살림",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "대놓고 두집살림",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202510/1760924755891613473.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202510/1760924757708636678.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202510/1760924762989390738.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202510/1760924762989390738.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202510/1760924791668522391.png",
+    targetage: "15",
+    actors: ["장윤정", "장동민", "홍현희", "도경완", "제이쓴"],
+    directors: ["정동현", "황민지", "양상혁", "양태열"],
+    tags: ["가족", "라이프"],
+    season_writers: [],
+    tmdb_id: 303592,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 1st (series_title) (Used title: 대놓고 두집살림)",
+  },
+  {
+    index: 4,
+    series_title: "크라임 씬3",
+    series_refer_id: "J3801_J38000000051",
+    content_refer_id: "J3801_J38000000051_01_0000.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "크라임 씬",
+    seasonnumbertitle: "3",
+    context_type: "vod",
+    seasontitle: "크라임 씬3",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202509/1758068932690895088.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202509/1758068937908817167.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202509/1758068945108269748.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202509/1758068945108269748.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202509/1758068986918036230.png",
+    targetage: "15",
+    actors: ["장진", "박지윤", "김지훈", "양세형", "정은지"],
+    directors: ["윤현준"],
+    tags: ["기타"],
+    season_writers: [],
+    tmdb_id: 214180,
+    tmdb_search_status:
+      "Closest match for '크라임 씬: 텍사스 킬링 필드' (Title Mismatch, vod) | Stage: 2nd (programtitle) (Used title: 크라임 씬)",
+  },
+  {
+    index: 5,
+    series_title: "길바닥 밥장사",
+    series_refer_id: "J3801_J38000000035",
+    content_refer_id: "J3801_J38000000035_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "길바닥 밥장사",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "길바닥 밥장사",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202509/1758069688826428603.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202509/1758069698776457883.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202509/1758069706256464522.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202509/1758069706256464522.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202509/1758069755469033726.png",
+    targetage: "15",
+    actors: ["류수영"],
+    directors: ["김소영", "조동휘"],
+    tags: ["리얼리티", "푸드"],
+    season_writers: [],
+    tmdb_id: 288474,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 1st (series_title) (Used title: 길바닥 밥장사)",
+  },
+  {
+    index: 6,
+    series_title: "비긴어게인 인터미션",
+    series_refer_id: "J3801_J38000000096",
+    content_refer_id: "J3801_J38000000096_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "비긴어게인",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "비긴어게인 인터미션",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202511/1763364404368794122.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202511/1763364410049979012.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202511/1763364422644382270.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202511/1763364422644382270.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202511/1763364470519862726.png",
+    targetage: "15",
+    actors: [],
+    directors: ["신영광"],
+    tags: ["뮤직", "토크"],
+    season_writers: [],
+    tmdb_id: 73068,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 2nd (programtitle) (Used title: 비긴어게인)",
+  },
+  {
+    index: 7,
+    series_title: "효리네 민박2",
+    series_refer_id: "J3801_J38000000058",
+    content_refer_id: "J3801_J38000000058_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "효리네 민박",
+    seasonnumbertitle: "2",
+    context_type: "vod",
+    seasontitle: "효리네 민박2",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202510/1760408202985458713.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202510/1760408209344342657.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202510/1760408214399499773.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202510/1760408214399499773.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202510/1760408250301051292.png",
+    targetage: "15",
+    actors: ["이효리", "이상순", "윤아"],
+    directors: ["정효민"],
+    tags: ["버라이어티", "리얼리티"],
+    season_writers: [],
+    tmdb_id: 72763,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 2nd (programtitle) (Used title: 효리네 민박)",
+  },
+  {
+    index: 8,
+    series_title: "1호가 될 순 없어2",
+    series_refer_id: "J3801_J38000000013",
+    content_refer_id: "J3801_J38000000013_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "1호가 될 순 없어",
+    seasonnumbertitle: "2",
+    context_type: "vod",
+    seasontitle: "1호가 될 순 없어2",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202508/1754530642654857269.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202508/1754530653496080293.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202508/1754530659170454018.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202508/1754530659170454018.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202508/1754530742246348352.png",
+    targetage: "15",
+    actors: ["최양락", "팽현숙"],
+    directors: [],
+    tags: ["리얼리티", "가족"],
+    season_writers: [],
+    tmdb_id: 104612,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 2nd (programtitle) (Used title: 1호가 될 순 없어)",
+  },
+  {
+    index: 9,
+    series_title: "한끼합쇼",
+    series_refer_id: "J3801_J38000000012",
+    content_refer_id: "J3801_J38000000012_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "한끼합쇼",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "한끼합쇼",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202508/1754528332473047550.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202508/1754528340272941823.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202508/1754528348151238947.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202508/1754528348151238947.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202508/1754528411758974104.png",
+    targetage: "15",
+    actors: ["김희선", "탁재훈"],
+    directors: ["민철기", "김수진", "권예솔", "정현희", "윤기준"],
+    tags: ["토크", "푸드"],
+    season_writers: [],
+    tmdb_id: 295865,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 1st (series_title) (Used title: 한끼합쇼)",
+  },
+  {
+    index: 10,
+    series_title: "히든싱어7",
+    series_refer_id: "J3801_J38000000071",
+    content_refer_id: "J3801_J38000000071_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "히든싱어",
+    seasonnumbertitle: "7",
+    context_type: "vod",
+    seasontitle: "히든싱어7",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202510/1761206628421426239.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202510/1761206634154067761.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202510/1761206639900740533.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202510/1761206639900740533.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202510/1761206696196019007.png",
+    targetage: "15",
+    actors: ["전현무"],
+    directors: ["조승욱"],
+    tags: ["뮤직", "경연"],
+    season_writers: [],
+    tmdb_id: 72993,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 2nd (programtitle) (Used title: 히든싱어)",
+  },
+  {
+    index: 11,
+    series_title: "뭉쳐야 찬다4",
+    series_refer_id: "J3801_J38000000004",
+    content_refer_id: "J3801_J38000000004_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "뭉쳐야 찬다",
+    seasonnumbertitle: "4",
+    context_type: "vod",
+    seasontitle: "뭉쳐야 찬다4",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202511/1763096829790735316.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202511/1763096834672656033.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202511/1763096837054761207.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202511/1763096837054761207.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202511/1763096907722646808.png",
+    targetage: "15",
+    actors: ["박항서", "안정환", "김남일", "이동국", "김성주", "김용만"],
+    directors: ["성치경"],
+    tags: ["스포츠"],
+    season_writers: [],
+    tmdb_id: 90534,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 2nd (programtitle) (Used title: 뭉쳐야 찬다)",
+  },
+  {
+    index: 12,
+    series_title: "아는 형님",
+    series_refer_id: "J3801_J38000000019",
+    content_refer_id: "J3801_J38000000019_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "아는 형님",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "아는 형님",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202508/1754526842726264387.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202508/1754526849183529295.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202508/1754526856917371975.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202508/1754526856917371975.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202508/1754527052268903638.png",
+    targetage: "15",
+    actors: [
+      "강호동",
+      "서장훈",
+      "김영철",
+      "이수근",
+      "김희철",
+      "민경훈",
+      "이상민",
+    ],
+    directors: ["최창수"],
+    tags: ["토크"],
+    season_writers: [],
+    tmdb_id: 70672,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 1st (series_title) (Used title: 아는 형님)",
+  },
+  {
+    index: 13,
+    series_title: "싱어게인4",
+    series_refer_id: "J3801_J38000000074",
+    content_refer_id: "J3801_J38000000074_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "싱어게인",
+    seasonnumbertitle: "4",
+    context_type: "vod",
+    seasontitle: "싱어게인4",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202510/1760513281100245976.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202510/1760513294544190173.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202510/1760513300708578628.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202510/1760513300708578628.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202510/1760513329424178337.png",
+    targetage: "15",
+    actors: [
+      "이승기",
+      "임재범",
+      "윤종신",
+      "백지영",
+      "김이나",
+      "규현",
+      "태연",
+      "이해리",
+      "코드 쿤스트",
+    ],
+    directors: ["윤현준", "위재혁"],
+    tags: ["오디션", "뮤직"],
+    season_writers: [],
+    tmdb_id: 303308,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 1st (series_title) (Used title: 싱어게인4)",
+  },
+  {
+    index: 14,
+    series_title: "비긴어게인 오픈마이크",
+    series_refer_id: "J3801_J38000000018",
+    content_refer_id: "J3801_J38000000018_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "비긴어게인 오픈마이크",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "비긴어게인 오픈마이크",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202508/1755153445374079627.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202508/1755153453270209605.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202508/1755153459233717550.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202508/1755153459233717550.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202508/1755153515894012251.png",
+    targetage: "15",
+    actors: [],
+    directors: ["송광종"],
+    tags: ["뮤직", "토크"],
+    season_writers: [],
+    tmdb_id: 136254,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 1st (series_title) (Used title: 비긴어게인 오픈마이크)",
+  },
+  {
+    index: 15,
+    series_title: "냉장고를 부탁해 since2014",
+    series_refer_id: "J3801_J38000000003",
+    content_refer_id: "J3801_J38000000003_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "냉장고를 부탁해",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "냉장고를 부탁해 since2014",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202506/1749708378775527490.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202506/1749708380743458971.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202506/1749708386596512303.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202506/1749708386596512303.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202506/1749708415882631797.png",
+    targetage: "15",
+    actors: ["김성주", "안정환"],
+    directors: ["이창우", "이린하"],
+    tags: ["푸드", "토크"],
+    season_writers: [],
+    tmdb_id: 73036,
+    tmdb_search_status:
+      "No results found for series_title, programtitle, or seasontitle (vod)",
+  },
+  {
+    index: 16,
+    series_title: "이혼숙려캠프",
+    series_refer_id: "J3801_J38000000002",
+    content_refer_id: "J3801_J38000000002_01_0001.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "이혼숙려캠프",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "이혼숙려캠프",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202506/1749708100938435091.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202506/1749708102760974518.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202506/1749708108917771347.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202506/1749708108917771347.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202506/1749708147526744466.png",
+    targetage: "15",
+    actors: ["서장훈", "박하선", "진태현"],
+    directors: ["김민종"],
+    tags: ["기타", "가족"],
+    season_writers: [],
+    tmdb_id: 262400,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 1st (series_title) (Used title: 이혼숙려캠프)",
+  },
+  {
+    index: 17,
+    series_title: "톡파원 25시",
+    series_refer_id: "J3801_J38000000020",
+    content_refer_id: "J3801_J38000000020_01_0121.1",
+    channelname: "JTBC",
+    genretext: "예능",
+    programtitle: "톡파원 25시",
+    seasonnumbertitle: "",
+    context_type: "vod",
+    seasontitle: "톡파원 25시",
+    season_horizontal_logoY_image:
+      "image.wavve.com/meta/image/202508/1755583763195610469.jpg",
+    season_horizontal_logoN_image:
+      "image.wavve.com/meta/image/202508/1755583771991878689.jpg",
+    season_vertical_logoY_image:
+      "image.wavve.com/v1/thumbnails/480_720_20_80/meta/image/202508/1755583778916696515.jpg",
+    seasonposterimage:
+      "image.wavve.com/meta/image/202508/1755583778916696515.jpg",
+    seasontitlelogoimage:
+      "image.wavve.com/meta/image/202508/1755583835668416143.png",
+    targetage: "15",
+    actors: ["전현무", "김숙", "양세찬", "이찬원"],
+    directors: ["홍상훈", "오상석", "고혁준", "김지수", "강영현"],
+    tags: ["여행", "토크"],
+    season_writers: [],
+    tmdb_id: 156845,
+    tmdb_search_status:
+      "Matched by name (vod) | Stage: 1st (series_title) (Used title: 톡파원 25시)",
+  },
+];
