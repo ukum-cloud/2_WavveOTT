18c2e8f83739f65722a08d4b6dbab412a6b903a0
amesystand <amesystand@gmail.com>
2025-12-24 11:41:32 +0900

    모바일업로드 아이콘추가 및 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,38 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        // 1. 경고 제한 수치를 500kb에서 1000kb(1MB)로 상향
-        chunkSizeWarningLimit: 1000,
-
-        // 2. 대형 라이브러리(vendor) 분리 설정
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        // node_modules에 있는 것들은 별도의 파일로 쪼갭니다.
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                },
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Wavve App',
+                short_name: 'Wavve',
+                start_url: '/',
+                display: 'standalone',
+                // 화면이 열릴때 보이는 배경
+                background_color: '#ffffff',
+                theme_color: '#000000',
+                icons: [
+                    {
+                        // src: "/Wavve_192x192.svg",
+                        // sizes: "192x192",
+                        // type: "image/svg+xml",
+                        src: '/Wavve_192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: '/Wavve_512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable', //
+                    },
+                ],
             },
-        },
-    },
+        }),
+    ],
 });
